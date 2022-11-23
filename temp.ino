4fd4925fac9283f6dc9f1e09dc6d8d5e9ed7762c
Imani-ES <imanimuh@buffalo.edu>
2022-11-23 15:06:18 -0500

    Update temp.ino

--- temp.ino
+++ temp.ino
@@ -50,7 +50,7 @@ String readDHTHumidity() {
   }
 }
 
-const char index_html[] PROGMEM = R"rawliteral(
+const char sensors_html[] PROGMEM = R"rawliteral(
 <!DOCTYPE HTML><html>
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -75,6 +75,7 @@ const char index_html[] PROGMEM = R"rawliteral(
 <body>
   <h2>Imani's IOT Project</h2>
   <p>
+    Sensors
     <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
     <span class="dht-labels">Temperature</span> 
     <span id="temperature">%TEMPERATURE%</span>
@@ -86,6 +87,11 @@ const char index_html[] PROGMEM = R"rawliteral(
     <span id="humidity">%HUMIDITY%</span>
     <sup class="units">&percnt;</sup>
   </p>
+  
+  <button onclick = "go_home()"> Home </button>   
+  <button onclick = "go_sensors()"> Home </button>   
+  <button onclick = "go_about()"> Home </button> 
+  <button onclick = "go_actuators()"> Home </button> 
 </body>
 <script>
 setInterval(function ( ) {
@@ -136,41 +142,110 @@ const char about_html[] PROGMEM = R"rawliteral(
 </head>
 <body>
   <h2>Imani's IOT Project</h2>
+  <p>About</p>
   <p>
-    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i> 
-    <span class="dht-labels">Temperature</span> 
-    <span id="temperature">%TEMPERATURE%</span>
-    <sup class="units">&deg;C</sup>
+    Iot project using the ESP32 as an edge device and server
   </p>
-  <p>
-    <i class="fas fa-tint" style="color:#00add6;"></i> 
-    <span class="dht-labels">Humidity</span>
-    <span id="humidity">%HUMIDITY%</span>
-    <sup class="units">&percnt;</sup>
-  </p>
+  
+  <button onclick = "go_home()"> Home </button>   
+  <button onclick = "go_sensors()"> Home </button>   
+  <button onclick = "go_about()"> Home </button> 
+  <button onclick = "go_actuators()"> Home </button> 
 </body>
 <script>
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("temperature").innerHTML = this.responseText;
-    }
-  };
-  xhttp.open("GET", "/temperature", true);
-  xhttp.send();
-}, 10000 ) ;
 
-setInterval(function ( ) {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("humidity").innerHTML = this.responseText;
+</script>
+</html>)rawliteral";
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
     }
-  };
-  xhttp.open("GET", "/humidity", true);
-  xhttp.send();
-}, 10000 ) ;
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>Imani's IOT Project</h2>
+  <p>Home</p>
+  <button onclick = "go_home()"> Home </button>   
+  <button onclick = "go_sensors()"> Home </button>   
+  <button onclick = "go_about()"> Home </button> 
+  <button onclick = "go_actuators()"> Home </button> 
+</body>
+<script>
+    var addy = '';
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            addy = this.responseText;
+        }
+    };
+    xhttp.open("GET", "/address", true);
+    xhttp.send();
+    function go_home() {
+        window.location.href = addy +'/home';
+    }
+    function go_sensors() {
+        window.location.href = addy +'/sensors';
+    }
+    function go_about() {
+        window.location.href = addy +'/about';
+    }
+    function go_actuators() {
+        window.location.href = addy +'/actuators';
+    }
+</script>
+</html>)rawliteral";
+
+const char actuators_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <style>
+    html {
+     font-family: Arial;
+     display: inline-block;
+     margin: 0px auto;
+     text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>Imani's IOT Project</h2>
+  <p>Actuators</p>
+
+  
+  <button onclick = "go_home()"> Home </button>   
+  <button onclick = "go_sensors()"> Home </button>   
+  <button onclick = "go_about()"> Home </button> 
+  <button onclick = "go_actuators()"> Home </button> 
+</body>
+<script>
+
 </script>
 </html>)rawliteral";
 
@@ -187,39 +262,49 @@ String processor(const String& var){
 }
 
 void setup(){
-  // Setup LEDs
-  ledcAttachPin (led_pin, 0);
-  ledcSetup (0,5000,8);
+// Setup LEDs
+    ledcAttachPin (led_pin, 0);
+    ledcSetup (0,5000,8);
 
-  // Serial port for debugging purposes
-  Serial.begin(115200);
+// Serial port for debugging purposes
+    Serial.begin(115200);
 
-  dht.begin();
-  
-  // Connect to Wi-Fi
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.println("Connecting to WiFi ..");
-  }
+    dht.begin();
 
-  // Print ESP32 Local IP Address
-  Serial.print("We're in boys... find me at ");
-  Serial.println(WiFi.localIP());
+// Connect to Wi-Fi
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(1000);
+        Serial.println("Connecting to WiFi ..");
+    }
+    Serial.print("We're in boys... find me at ");
+    Serial.println(WiFi.localIP());
 
-  // Route for root / web page
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/html", index_html, processor);
-  });
-  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTTemperature().c_str());
-  });
-  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send_P(200, "text/plain", readDHTHumidity().c_str());
-  });
+// SERVER FUNCTIONALITY
+    //PAGES
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", index_html, processor);
+    });
+    server.on("/about", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", about_html, processor);
+    });
+    server.on("/sensors", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", sensors_html, processor);
+    });
+    server.on("/actuators", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/html", actuators_html, processor);
+    });
 
-  // Start server
-  server.begin();
+    // FEATURES
+    server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", readDHTTemperature().c_str());
+    });
+    server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", readDHTHumidity().c_str());
+    });
+
+    // Start server
+    server.begin();
 }
 
 void breathe(){
