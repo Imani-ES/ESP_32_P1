d018926f3f039ed6c9a497e2c90ab5830649179e
Imani-ES <imanimuh@buffalo.edu>
2022-11-29 18:08:50 -0500

    Final Code
    Works, Going to edit for submission and Demo

--- temp.ino
+++ temp.ino
@@ -1,3 +1,5 @@
+//Imani Muhammad-Graham
+
 #include <DNSServer.h>
 
 // Import required libraries
@@ -5,6 +7,8 @@
 #include "ESPAsyncWebServer.h"
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
+#include <Stepper.h>
+
 
 // Network Credentials
 const char* ssid = "UB_Devices";
@@ -18,37 +22,137 @@ AsyncWebServer server(80);
 #define DHTTYPE    DHT11     
 DHT dht(DHTPIN, DHTTYPE);
 
-//Light Show
-const byte led_pin = 2;
-int brightness = 0;
-int fade_rate = 5;
-int light_show = 0;
-
+//Get Temperature
 String readDHTTemperature() {
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float t = dht.readTemperature(true);
   // Check if any reads failed and exit early (to try again).
   if (isnan(t)) {    
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("readDHTTemperature -> Failed to read from DHT sensor!");
     return "--";
   }
   else {
+    Serial.print("readDHTTemperature -> ");
     Serial.println(t);
     return String(t);
   }
 }
 
+//Get Humidity
 String readDHTHumidity() {
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   if (isnan(h)) {
-    Serial.println("Failed to read from DHT sensor!");
+    Serial.println("readDHTHumidity ->  Failed to read from DHT sensor!");
     return "--";
   }
   else {
+    Serial.print("readDHTHumidity -> ");
     Serial.println(h);
     return String(h);
   }
+}
+
+//Light Show Setup
+const byte led_pin = 2;
+int brightness = 0;
+int fade_rate = 5;
+int light_show = 0;
+
+//Switch Light shows
+String light_show_switch(){  
+  Serial.println("Switch Light show");
+  light_show += 1;
+  if (light_show >2){
+    //Only 3 light shows (including 0)
+    light_show = 0;
+  }
+  //return current light show
+  char ret[256];
+  itoa(light_show,ret,10);
+  
+  Serial.printf("light_show_switch -> Current Light Show: %d\n",light_show);
+  return ret;
+}
+
+// Light Show 0
+void breathe(){
+  ledcWrite(0,brightness);
+  brightness = brightness + fade_rate;
+  if (brightness >= 255 || brightness <= 0) {
+    fade_rate = -fade_rate;
+  }
+}
+
+//Light Show 1
+void blink(){
+  ledcWrite(0,brightness);
+  brightness = brightness + fade_rate*10;
+  if (brightness >= 255 || brightness <= 0) {
+    fade_rate = -fade_rate;
+  }
+}
+
+//Light show 2
+void pulse(){
+  ledcWrite(0,brightness);
+  brightness = brightness + fade_rate*5;
+  if (brightness >= 255 || brightness <= 0) {
+    fade_rate = -fade_rate;
+  }
+}
+
+//DC Motor Driver Setup
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+
+//DC Motor Set up
+const int stepspr = 2048; //Steps for revolution
+int spr = 2048;
+int dc_speed = 1; //Motor speed
+int max_dc_speed = 15; // Max Speed
+Stepper dc_motor(stepspr, IN1, IN3, IN2, IN4);//Instantiate DC Motor
+
+//Motor speed change functionality
+String motor_speed(int speed){
+  dc_speed += speed;
+
+  //Negative direction
+  if (dc_speed < 0){
+    //Switch direction
+    if (spr > 0){     
+      spr = -spr;
+    }
+    //Speed limit
+    if (dc_speed <= -max_dc_speed){
+      dc_speed = -max_dc_speed;
+    }
+  } 
+
+  //Positive direction
+  if (dc_speed >=0){    
+    //Switch Direction
+    if (spr <0){
+      spr = -spr;
+    }
+
+    //error when speed = 0, so just have it at 1
+    if (dc_speed == 0){
+      dc_speed = 1;
+    }
+
+    //Speed Limit
+    if (dc_speed >= max_dc_speed) {
+      dc_speed = max_dc_speed;
+    }
+  } 
+  dc_motor.setSpeed(abs(dc_speed));
+  Serial.printf("motor_speed -> Current Speed: %d\n",dc_speed);
+  char ret[256];
+  itoa(dc_speed,ret,10);
+  return(ret);
 }
 
 const char index_html[] PROGMEM = R"rawliteral(
@@ -113,27 +217,59 @@ const char index_html[] PROGMEM = R"rawliteral(
 
     <h4> Light show </h4>
     <span id="light_show"> Choose a Light Show </span>
-    <button onclick = "light_show()"> Switch Light Show </button>  
+    <button onclick = "handler(0)"> Switch Light Show </button>  
 
     <h4> Fan </h4>
     <span id="motor"> Choose a Fan setting </span>
-    <button onclick = "motor_show()"> Change Fan setting </button>  
-
+    <button onclick = "handler(1)"> Speed up Fan </button>  
+    <button onclick = "handler(2)"> Slow down Fan </button>  
   </div>
 
 </body>
 <script>  
-  function light_show(){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-          if (this.responseText == "Success"){
-            document.getElementById("light_show").innerHTML = this.responseText;
+  function handler(n){
+    //Light Show
+    if (n == 0) {
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText == "Success"){
+              document.getElementById("light_show").innerHTML = this.responseText;
+            }
           }
-        }
-    };
-    xhttp.open("GET", "/light_show_1", true);
-    xhttp.send();
+      };
+      xhttp.open("GET", "/light_show", true);
+      xhttp.send();
+    }
+    //Motor Speed
+    else if (n == 1){
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText == "Success"){
+              document.getElementById("motor").innerHTML = this.responseText;
+            }
+          }
+      };
+      xhttp.open("GET", "/motor_speed", true);
+      xhttp.send();
+    }
+    //Motor Slow
+    else if (n == 2){
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+          if (this.readyState == 4 && this.status == 200) {
+            if (this.responseText == "Success"){
+              document.getElementById("motor").innerHTML = this.responseText;
+            }
+          }
+      };
+      xhttp.open("GET", "/motor_slow", true);
+      xhttp.send();        
+    }
+    else{
+      alert("Incorrect Input")
+    }
   }
 
   setInterval(function ( ) {
@@ -180,7 +316,7 @@ void setup() {
 
   // Serial port for debugging purposes
     Serial.begin(115200);
-
+  //Set up Sensors
     dht.begin();
 
   // Connect to Wi-Fi
@@ -191,7 +327,7 @@ void setup() {
     }
     Serial.print("We're in boys... find me at ");
     Serial.println(WiFi.localIP());
-
+  
   // SERVER FUNCTIONALITY
     //PAGES
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -204,64 +340,30 @@ void setup() {
     server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request){
         request->send_P(200, "text/plain", readDHTHumidity().c_str());
     });
-    server.on("/light_show_1", HTTP_GET, [](AsyncWebServerRequest *request){
-        //light_show_1();     
-        request->send_P(200, "text/plain", "Light Show 1");
+    server.on("/light_show", HTTP_GET, [](AsyncWebServerRequest *request){
+             
+        request->send_P(200, "text/plain", light_show_switch().c_str());
     });
-    server.on("/light_show_2", HTTP_GET, [](AsyncWebServerRequest *request){
-        //light_show_2();
-        request->send_P(200, "text/plain", "Light Show 2");
+    server.on("/motor_slow", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", motor_speed(-5).c_str());
+    });
+    server.on("/motor_speed", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send_P(200, "text/plain", motor_speed(5).c_str());
     });
 
     // Start server
     server.begin();
 }
 
-int light_show_1(){
-  //turn to light show 1
-  Serial.println("Switched to Light show 1");
-  light_show = 1;
-  return 1;
-}
 
-int light_show_2(){
-  //turn to light show 2  
-  light_show = 2;
-  Serial.println("Switched to Light show 2");
-  return 1;
-}
-
-void breathe(){
-  // Light Show 0
-  ledcWrite(0,brightness);
-  brightness = brightness + fade_rate;
-  if (brightness >= 255 || brightness <= 0) {
-    fade_rate = -fade_rate;
-  }
-}
-
-void blink(){
-  //Light Show 1
-  ledcWrite(0,brightness);
-  brightness = brightness + fade_rate*10;
-  if (brightness >= 255 || brightness <= 0) {
-    fade_rate = -fade_rate;
-  }
-}
-
-void pulse(){
-  //Light show 2
-  ledcWrite(0,brightness);
-  brightness = brightness + fade_rate*5;
-  if (brightness >= 255 || brightness <= 0) {
-    fade_rate = -fade_rate;
-  }
-}
 
 void loop(){
   // Light Show
   if (light_show == 0){breathe();}
   else if (light_show == 1){blink();}
   else{pulse();}
-  delay(50);
+
+  //Motor 
+  dc_motor.step(spr);
+  delay(25);
 }
